0364cb5cd1fe476f0e23476c4ddeeab2553e5814
Ufuk Tandogan <ufuktandogan@windowslive.com>
2014-07-03 17:39:50 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThingsIBought::Application.config.secret_key_base = 'e7e2d0bd4a2933592e12fc632e9548d9e2d7636779325520ed4f260922a9fe8fdcce537a9cc1af358a6999b6087095e8f83c336c787f681f76d25ae6a8fc47e1'
